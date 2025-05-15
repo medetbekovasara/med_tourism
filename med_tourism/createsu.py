60028d8735f0e57f9f67dddb6e0224999c3e119d
awesomesara <medetbekovasara1@gmail.com>
2025-05-15 17:40:11 +0600

    деплой

--- med_tourism/createsu.py
+++ med_tourism/createsu.py
@@ -5,11 +5,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'med_tourism.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    user = User.objects.create_superuser('admin', 'admin@example.com', 'admin')
     print('✔️ Суперюзер создан: admin/admin')
 else:
     print('⚠️ Суперюзер уже существует')
+    user = User.objects.get(username='admin')
+    user.is_superuser = True
+    user.is_staff = True
+    user.set_password('admin')
+    user.save()
+    print('✅ Данные суперюзера обновлены')
